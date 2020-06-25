66d8d9f4be4b6f9915af40de7389107511918c6e
sunlina <2280294941@qq.com>
2020-06-25 13:09:11 +0800

    haha

--- jq/detail.js
+++ jq/detail.js
@@ -3,10 +3,12 @@ $(function(){
     var asmall = $(".prol-smallimg").find("img");
     var bigimg = $(".prol-img").find(".swiper");
 
-    asmall.click(function(){
+    asmall.hover(function(){
         asmall.attr("class", "");
         bigimg.css("display", 'none');
-        $(this).attr("class", "active");
+        $(this).attr("class", "active").css("border-color","#f60");
         bigimg.eq($(this).index()).css("display", 'block');
+    },function(){
+        asmall.css("border-color","none")
     })
 })
